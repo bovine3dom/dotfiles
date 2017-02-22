3f3e8749eba05bf2bda9fdc87bf314dfdb1d3795
Oliver Blanthorn <freedom4cows@gmail.com>
2017-02-22 19:36:20 +0000

    Change vimget to use yadm submodules

--- .config/fish/functions/vimget.fish
+++ .config/fish/functions/vimget.fish
@@ -1,3 +1,3 @@
 function vimget --description 'alias vimget cd ~/.vim/bundle; git clone --depth=1 '
-	cd ~/.vim/bundle; git clone --depth=1  $argv; cd -;
+	cd ~/.vim/bundle; yadm submodule add --depth=1  $argv; cd -;
 end
