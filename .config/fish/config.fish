e23c2c943dd54d1319daff2b10a3c62e76a6c3e1
Oliver Blanthorn <freedom4cows@gmail.com>
2018-12-13 20:45:40 +0000

    Break git diff on most of my machines

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,19 +3,20 @@ setenv EDITOR /usr/bin/vim
 setenv GPG_TTY (tty)
 
 
-fzf_key_bindings
 
 . ~/.config/fish/functions/z.fish
 
 if status --is-login
+    set PATH $PATH /usr/bin /sbin ~/bin ~/.gem/ruby/2.4.0/bin ~/.gem/ruby/2.5.0/bin
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
         # exec startx -- -keeptty
     end
 end
 
 set BROWSER /usr/bin/firefox
+set GDK_SCALE 2
 
 keychain --quiet -Q --agents ssh ~/.ssh/*_rsa
 bass '. ~/.keychain/$HOSTNAME-sh'
 
-set PATH $PATH /usr/bin /sbin ~/bin ~/.gem/ruby/2.5.0/bin
+fzf_key_bindings
