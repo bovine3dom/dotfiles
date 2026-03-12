da9d97c3f66e42d2a14d4b5915da2484ca0d2090
Oliver Blanthorn <freedom4cows@gmail.com>
2026-03-12 12:06:47 +0100

    Add wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.font_size = 15
+config.font = wezterm.font "Iosevka Term"
+
+config.color_scheme = 'Google (light) (terminal.sexy)'
+
+-- make fonts less blurry
+config.freetype_load_target = "Light"
+config.freetype_load_flags = "FORCE_AUTOHINT"
+
+config.enable_tab_bar = false
+
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
+return config
